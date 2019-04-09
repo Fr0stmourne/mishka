532dfa60b418c2d019dbcfd2cb50364183425836
Roman <cozroman@yandex.ru>
2019-04-09 14:41:26 +0300

    deleted extra files + installed packages

--- gulpfile.js
+++ gulpfile.js
@@ -8,25 +8,25 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
 gulp.task("style", function () {
-  gulp.src("sass/style.scss")
+  gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
 gulp.task("serve", ["style"], function () {
   server.init({
-    server: ".",
+    server: "source/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("source/*.html").on("change", server.reload);
 });
